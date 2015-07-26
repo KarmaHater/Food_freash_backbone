01565a1f2b8dc1a8427758dff0e8bd87e6065f90
Adnard-kod <andra.lally@gmail.com>
2015-07-26 11:05:09 -0500

    render recipes

--- app/components/js/recipes_view.js
+++ app/components/js/recipes_view.js
@@ -1,19 +1,55 @@
-$(document).ready(function() {
+$(document).ready(function(){
+
   var RecipeView = Backbone.View.extend({
     template: $("#recipe-tmp").html(),
     render: function() {
-      var tmpl = _.template(this.template);
-      this.$el.html(tmp(this.model.toJSON()));
+      var tmpl = Handlebars.compile(this.template);
+      tmpl(this.model.toJSON())
+      this.$el.html(tmpl(this.model.toJSON()));
+      return this;
     }
   })
 
   var RecipesView = Backbone.View.extend({
     el: $("#recipesList"),
     initialize: function() {
-      // this.collection: new RecipesCollection(recipes);
+      this.collection = new RecipesCollection();
     },
     render: function() {
-      
+      for (var i = 0; i < this.collection.models.length; i++) {
+        var rView = new RecipeView({ model: this.collection.models[i] })
+        this.$el.append(rView.render().el);
+      };
+    },
+    start: function() {
+      $.ajax({
+        url: '../../recipes.json'
+      })
+      .success( function(data) {
+        this.createRecipes(data);
+        this.render();
+      }.bind(this))
+    },
+    createRecipes: function(recipes) {
+      for (var i = 0; i < recipes.length; i++) {
+        var id = counter;
+        var name = recipes[i].name;
+        var difficulty = recipes[i].difficulty;
+        var rating = recipes[i].rating;
+        var image = recipes[i].image;
+        var headline = recipes[i].headline;
+        var description = recipes[i].description;
+        var recipe_id= recipes[i].recipes_id
+        var deliverable_ingredients = recipes[i].deliverable_ingredients;
+        var ingredients = recipes[i].ingredients;
+        var calories = recipes[i].calories;
+        var fats = recipes[i].fats;
+        var proteins = recipes[i].proteins;
+        this.collection.push(recipes[i])
+      }
     }
   })
+
+  var view = new RecipesView;
+  view.start();
 })
